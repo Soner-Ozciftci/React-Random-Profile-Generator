4381c8779e5b9f5e78481f5740656183403b0e60
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-07-26 13:46:50 +0200

    add axios

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Uygulama da useEffect ve useState kullandım.Aynı zamanda veriyi 'axios' ile çektim
+verileri "randomuser.me" den aldım.
--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css'
 import axios from 'axios';
 
 function App() {
+
+const[ profileName,setProfileName]=useState("");
+const[ profileCell,setProfileCell]=useState("");
+const[ profileImage,setProfileImage]=useState("");
+const[ profileEmail,setProfileEmail]=useState("");
+
+const profileData = async()=>{
+
+try {
+  const res = await axios.get("https://randomuser.me/api");
+  console.log(res);
+} catch (error) {
+ console.log("error");
+  }
+}
   return (
     <div className="App">
 
