9a7bb6ddaa7d5c25dad63e95c178c484ef7b1f54
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-07-26 14:52:54 +0200

    readme add

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 Uygulama da useEffect ve useState kullandım.Aynı zamanda veriyi 'axios' ile çektim
 verileri "randomuser.me" den aldım.
+
+React Random Profile Generator App
+This app is a simple profile creation tool developed using React. It allows users to obtain test-purpose fake profiles by generating random profile information.
+
+Features:
+
+Creating random usernames, full names, emails, addresses, and profile pictures.
+Easy copy and paste of the generated profile information.
+User-friendly interface for effortless usage.
+Modular structure based on React components, enabling potential for adding more features.
--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import { useState , useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [profileName, setProfileName] = useState("");
+  const [profileCell, setProfileCell] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
 
-const[ profileName,setProfileName]=useState("");
-const[ profileCell,setProfileCell]=useState("");
-const[ profileImage,setProfileImage]=useState("");
-const[ profileEmail,setProfileEmail]=useState("");
-
-const profileData = async()=>{
-
-try {
-  const res = await axios.get("https://randomuser.me/api");
-  console.log(res);
-  setProfileCell(res.data.results[0].cell);
-  setProfileEmail(res.data.results[0].email);
-setProfileImage(res.data.results[0].picture.large);
-setProfileName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
-} catch (error) {
- console.log("error");
+  const profileData = async () => {
+    try {
+      const res = await axios.get("https://randomuser.me/api");
+      console.log(res);
+      setProfileCell(res.data.results[0].cell);
+      setProfileEmail(res.data.results[0].email);
+      setProfileImage(res.data.results[0].picture.large);
+      setProfileName(
+        `${res.data.results[0].name.first} ${res.data.results[0].name.last}`
+      );
+    } catch (error) {
+      console.log("error");
+    }
   }
-}
+  
+  useEffect(()=>{
+ profileData();
+},[])
+
   return (
     <div className="App">
-      <div className='profileContainer'>
-      <button onClick={profileData}>Click for new profile</button>
+      <div className="profileContainer">
+        <img src={profileImage}/>
+        <h1>{profileName}</h1>
+        <p>{profileEmail}</p>
+        <p>{profileCell}</p>
+        <button onClick={profileData}>Click for New Profile</button>
       </div>
-
     </div>
   );
 }
 
-export default App; 
+export default App;
